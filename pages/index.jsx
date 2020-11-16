e85cdc7cc903287073b095fe067570f054bf147d
guzilla(NALA) <13578427+gusvalenzuela@users.noreply.github.com>
2020-11-15 22:39:33 -0800

    Add meta tag w/ description. Add title and refresh button to NASA image

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,19 +9,23 @@ const APODQ = process.env.NEXT_PUBLIC_APODQ;
 
 const Homepage = () => {
   const [NASAImage, setNASAImage] = useState({});
-  const yearInMs = 1000 * 60 * 60 * 24 * 365;
-  var randomNum = Math.floor(Math.random() * yearInMs);
-  const randomDate = new Date(Date.now() - randomNum);
+  const [imageRefreshing, setImageRefreshing] = useState(false);
+  const yearInMilliseconds = 1000 * 60 * 60 * 24 * 365;
+  var randomNum = Math.floor(Math.random() * yearInMilliseconds);
+  const randomDate = new Date(Date.now() - randomNum); // random date between now and a year ago
 
   const getNasaImage = async () => {
+    setImageRefreshing(true);
     const data = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${APODQ}&hd=true&date=${
         randomDate.toISOString().split("T")[0]
       }`
     ).then((r) => r.json());
 
+    if (data) setImageRefreshing(false);
     setNASAImage(data);
   };
+  // on document load/change
   useEffect(() => {
     getNasaImage();
   }, [document]);
@@ -34,6 +38,10 @@ const Homepage = () => {
           href="https://fonts.googleapis.com/css2?family=Lobster&amp;family=Montserrat&amp;family=Open+Sans&amp;family=Poiret+One&amp;family=Roboto&amp;family=Special+Elite&amp;display=swap"
           rel="stylesheet"
         ></link>
+        <meta
+          name="description"
+          content="Gus Valenzuela's personal website displaying different web development skills and various links to other pages on the site."
+        ></meta>
       </Head>
       <MyHeader textContent="HOME" />
       <main className={Styles.homepageContainer} id="homepage-container">
@@ -41,19 +49,20 @@ const Homepage = () => {
         <div className={Styles.calloutIndex} id="callout">
           <div
             className={Styles.background}
-            style={{ backgroundImage: `url(${NASAImage.url})` }}
+            // style={{ backgroundImage: `url(${NASAImage.url})` }}
           >
-            {/* <img
+            <img
               src={NASAImage.hdurl}
               alt={NASAImage.title}
               name={NASAImage.title}
               title={NASAImage.explanation}
-            /> */}
+            />
           </div>
           <div className={Styles.foreground}>
             <div>
               <p className={Styles.headerIndex} id="header">
-                Gus Valenzuela
+                Gus <br />
+                Valenzuela
               </p>
               <p className={Styles.subheaderIndex} id="subheader">
                 At your service.
@@ -61,7 +70,7 @@ const Homepage = () => {
             </div>
             <div>
               <Link href="/portfolio" passHref>
-                <Button fluid color="red" animated>
+                <Button color="red" animated>
                   <Button.Content visible>Portfolio</Button.Content>
                   <Button.Content hidden>
                     <Icon name="arrow right" />
@@ -71,7 +80,7 @@ const Homepage = () => {
             </div>
             <div>
               <Link href="/contact" passHref>
-                <Button fluid color="red" animated>
+                <Button color="red" animated>
                   <Button.Content visible>Contact</Button.Content>
                   <Button.Content hidden>
                     <Icon name="arrow right" />
@@ -80,6 +89,30 @@ const Homepage = () => {
               </Link>
             </div>
           </div>
+        </div>
+        <div className={Styles.nasaImageContent}>
+          <h2>{NASAImage.title}</h2>
+          <h5>
+            {NASAImage.copyright && (
+              <>
+                <b>Copyright:</b> {NASAImage.copyright}
+              </>
+            )}
+          </h5>
+          <p>
+            The image above is from{" "}
+            <a
+              href="https://apod.nasa.gov/apod/astropix.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              NASA's Image of the Day
+            </a>
+            , fetched at random on each page load.
+            <a style={{ cursor: "pointer" }} onClick={getNasaImage}>
+              {!imageRefreshing ? " Refresh the image." : " Fetching new image."}
+            </a>
+          </p>
         </div>
         <div className={Styles.buttonsContainer}>
           <Link href="/about" passHref>
