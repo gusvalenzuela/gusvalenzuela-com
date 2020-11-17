c6cb20a8586f6cc8d753e03b40add3aeaee15454
guzilla <gusrosales7@gmail.com>
2020-11-17 14:33:22 -0800

    Initialize generic types file

---
+++ generic.d.ts
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+// ======================================================
+// Types
+// ======================================================
+
+export type GusProject = {
+  title: string;
+  version: string;
+  lead: string;
+  technologies: string;
+  description: string;
+  app_url: string;
+  repo_url: string;
+  repo_name: string;
+  img_src: string;
+  img_alt: string;
+};
+
+// ======================================================
+// Styling
+// ======================================================
