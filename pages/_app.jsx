52342bb8782dcc988f3194578f27d662ffd78768
guzilla <gusrosales7@gmail.com>
2021-08-11 12:28:31 -0700

    Remove moment and react-moment dependencies

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Moment from 'react-moment';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import Foot from '../components/Foot';
@@ -8,15 +7,12 @@ import '../styles/globals.css';
 import 'semantic-ui-css/semantic.min.css';
 
 export default function MyApp({ Component, pageProps }) {
-  // Start the pooled timer which runs every 60 seconds
-  // (60000 milliseconds) by default.
-  // more info: https://www.npmjs.com/package/react-moment#pooled-timer
-  Moment.startPooledTimer();
   // we must dynamically load the component using Next's dynamic
   const Sidebar = dynamic(() => import('../components/MySidebar'), {
     // turn server-side render off in order to access the window obj in browser API
     ssr: false,
   });
+
   return (
     <>
       {/* custom google fonts for header and others on index page */}
--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState, useEffect } from 'react';
-// import Moment from "react-moment";
 import Head from 'next/head';
 import { Dimmer, Loader } from 'semantic-ui-react';
 import API from '../utils/API';
