17759959089f01c5e0ce96c59687c7b6e01a91b2
gus valenzuela <gusrosales7@gmail.com>
2022-07-04 17:25:08 -0700

    Remove sidebar and move 'head' tag to _document page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
-import Head from 'next/head';
+// import dynamic from 'next/dynamic';
 import Foot from '../components/Foot';
+import NavMenu from '../components/NavMenu';
 // import BottomChevron from "../components/BottomChevron";
 import '../styles/globals.css';
 import 'semantic-ui-css/semantic.min.css';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({Component, pageProps}) {
   // we must dynamically load the component using Next's dynamic
-  const Sidebar = dynamic(() => import('../components/MySidebar'), {
-    // turn server-side render off in order to access the window obj in browser API
-    ssr: false,
-  });
+  // const Sidebar = dynamic(() => import('../components/MySidebar'), {
+  //   // turn server-side render off in order to access the window obj in browser API
+  //   ssr: false,
+  // });
 
   return (
-    <>
-      {/* custom google fonts for header and others on index page */}
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lobster&amp;family=Montserrat&amp;family=Open+Sans&amp;family=Poiret+One&amp;family=Roboto&amp;family=Special+Elite&amp;display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="grid-root">
-        <Sidebar>
-          <Component {...pageProps} />
-          <Foot />
-          {/* <BottomChevron visible={true} /> */}
-        </Sidebar>
-      </div>
-    </>
+    <div className="grid-root">
+      {/* <Sidebar> */}
+      <NavMenu />
+      <Component {...pageProps} />
+      <Foot />
+      {/* <BottomChevron visible={true} /> */}
+      {/* </Sidebar> */}
+    </div>
   );
 }
