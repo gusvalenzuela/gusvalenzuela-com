41cf9b8875686d3bf92579c068b6202eceaf353c
gus valenzuela <gusrosales7@gmail.com>
2022-11-15 18:15:25 -0800

    Redo navbar using semantic-ui library. Fix some styling

--- components/NavMenu/index.js
+++ components/NavMenu/index.js
@@ -1,36 +1,45 @@
+/* eslint-disable no-restricted-globals */
 import React from 'react';
-import Link from '../ActiveLink';
+import {useRouter} from 'next/router';
+import {Menu, Segment} from 'semantic-ui-react';
 import Styles from './NavMenu.module.css';
 
 function Nav() {
+  const {push, pathname} = useRouter();
+
+  const menuItem = React.useCallback(
+    (pagename) => {
+      const routeTo = (path) => {
+        push(path);
+      };
+
+      return (
+        <Menu.Item
+          className={Styles.menuItem}
+          active={pathname === `/${pagename}`}
+          onClick={() => routeTo(`/${pagename}`)}
+          link
+        >
+          {pagename || 'home'}
+        </Menu.Item>
+      );
+    },
+
+    [pathname, push],
+  );
+
   return (
-    <nav className={Styles.topNavMenu} id="topNavMenu">
-      <span className={Styles.brandName}>grv </span>
-      <span className={Styles.navDivider} />
-      <span>
-        <Link activeClassName="active"  href="/">
-          <a className={Styles.navMenuLinks}>home</a>
-        </Link>
-      </span>
-      <span className={Styles.navDivider} />
-      <span>
-        <Link activeClassName="active"  href="/portfolio">
-          <a className={Styles.navMenuLinks}>portfolio</a>
-        </Link>
-      </span>
-      <span className={Styles.navDivider} />
-      <span>
-        <Link activeClassName="active"  href="/contact">
-          <a className={Styles.navMenuLinks}>contact</a>
-        </Link>
-      </span>
-      <span className={Styles.navDivider} />
-      <span>
-        <Link activeClassName="active"  href="/video">
-          <a className={Styles.navMenuLinks}>video</a>
-        </Link>
-      </span>
-    </nav>
+    <Segment.Group className={Styles.topNavMenu} id="topNavMenu">
+      <Segment size="large" basic compact className={Styles.brandName}>
+        grv
+      </Segment>
+      <Menu pointing inverted widths={4}>
+        {menuItem('')}
+        {menuItem('portfolio')}
+        {menuItem('contact')}
+        {menuItem('video')}
+      </Menu>
+    </Segment.Group>
   );
 }
 
